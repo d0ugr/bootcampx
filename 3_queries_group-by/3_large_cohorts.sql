f774349368f865328a40a40b8e6efa7fa3ab9141
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-30 19:00:42 -0400

    Update 3_large_cohorts.sql

--- 3_queries_group-by/3_large_cohorts.sql
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -4,7 +4,7 @@
 SELECT   cohorts.name AS cohort_name, COUNT(students.id) AS student_count
 FROM     cohorts
 JOIN     students ON students.cohort_id = cohorts.id
-GROUP BY cohorts.name
+GROUP BY cohort_name
 HAVING   COUNT(students.id) >= 18
 ORDER BY student_count;
 
