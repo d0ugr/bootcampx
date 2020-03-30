94140e001fdf42149ac7eebdc3003d1035cb9c62
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-30 18:28:10 -0400

    Set column alias

--- 2_queries_joins/1_total_assignment_duration.sql
+++ 2_queries_joins/1_total_assignment_duration.sql
@@ -1,7 +1,7 @@
 
 
 
-SELECT SUM(duration)
+SELECT SUM(duration) as total_duration
 FROM   assignment_submissions
 JOIN   students ON student_id = students.id
 WHERE  students.name = 'Ibrahim Schimmel';
