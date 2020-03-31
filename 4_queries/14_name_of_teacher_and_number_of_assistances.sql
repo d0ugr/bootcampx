0f7f95cca25d9840a1284925dca3fbbe4076d6cb
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-31 14:56:03 -0400

    Add 14_name_of_teacher_and_number_of_assistances.sql

---
+++ 4_queries/14_name_of_teacher_and_number_of_assistances.sql
@@ -0,0 +1,14 @@
+
+
+
+SELECT   teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests) AS total_assistances
+FROM     cohorts
+JOIN     students ON students.cohort_id = cohorts.id
+JOIN     assistance_requests ON assistance_requests.student_id = students.id
+JOIN     teachers ON teachers.id = assistance_requests.teacher_id
+WHERE    cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teachers.name;
+
+
+
