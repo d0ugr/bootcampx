a48a4dc974fc6a9e233537b61bb36109f36e46b2
e-s-c-m <e-s-c-m@Betsy.lan>
2020-03-31 10:53:13 -0400

    Add 1_total_teacher_assistance_requests.sql

---
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -0,0 +1,11 @@
+
+
+
+SELECT   COUNT(assistance_requests.id), teachers.name
+FROM     assistance_requests
+JOIN     teachers ON teachers.id = assistance_requests.teacher_id
+WHERE    teachers.name = 'Waylon Boehm'
+GROUP BY teachers.name;
+
+
+
